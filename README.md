3aa53aee7a250c5a2d0060d064b9b8ffb82b8fe8
Muhammet Mert POLAT <mrtplt024@gmail.com>
2021-11-19 03:12:18 +0300

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# son-depremler-console
- C# console application that pulls and displays earthquakes in Turkey from the website.
+# Son Depremler C# Console Application
+
+C# console application that pulls and displays earthquakes in Turkey from the website.
+
+  - Language: C#
+
+# Installation
+
+- Run SonDeprem.sln
+- Debug the project.
+
+### Tech
+
+Application uses a number of modules to work properly:
+
+* C# - Programming language
+
+License
+----
+
+MIT
+
+### Developer
+
+- [Muhammet Mert POLAT](https://mertpolat.com.tr)
---
+++ SonDeprem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SonDeprem
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string VeriCek(string url, string etiket)
+            {
+                
+                var startTag = "<" + etiket + ">"; 
+                int startIndex = url.IndexOf(startTag) + startTag.Length;
+                int endIndex = url.IndexOf("</" + etiket + ">", startIndex);
+                return url.Substring(startIndex, endIndex - startIndex);
+            }
+
+            WebClient client = new WebClient();
+            string boun = client.DownloadString("http://www.koeri.boun.edu.tr/scripts/lst4.asp");
+
+            string result = VeriCek(boun, "pre").Remove(0, 250);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
